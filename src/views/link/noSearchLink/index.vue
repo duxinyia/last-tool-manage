293ea8d1de6b24bb6f87c3fc336fbb8b8dedbd85
duxinyi <G1656790@www.com>
2023-09-04 16:06:46 +0800

    增加料号详情弹窗

--- src/views/link/noSearchLink/index.vue
+++ src/views/link/noSearchLink/index.vue
@@ -1,48 +1,60 @@
 <template>
-	<div class="table-container layout-padding">
-		<el-card class="box-card">
-			<el-form ref="tableSearchRef" :model="state.form" size="default" label-width="auto" class="table-form">
-				<el-row>
-					<el-col
-						:xs="val.xs || 24"
-						:sm="val.sm || 12"
-						:md="val.md || 8"
-						:lg="val.lg || 6"
-						:xl="val.xl || 4"
-						class="mr20"
-						v-for="(val, key) in state.search"
-						:key="key"
+	<!-- <div class="table-container layout-padding"> -->
+	<!-- <el-card :class="isDialog ? '' : 'box-card'"> -->
+	<el-form v-if="state.search.length" ref="tableSearchRef" :model="state.form" size="default" label-width="auto" class="table-form">
+		<el-row>
+			<el-col
+				:xs="val.xs || 24"
+				:sm="val.sm || 12"
+				:md="val.md || 10"
+				:lg="val.lg || 8"
+				:xl="val.xl || 6"
+				class="mb20"
+				v-for="(val, key) in state.search"
+				:key="key"
+			>
+				<template v-if="val.type !== ''">
+					<el-form-item
+						:label="$t(val.label)"
+						:prop="val.prop"
+						:rules="[{ required: val.required, message: `${val.label}不能为空`, trigger: val.type === 'input' ? 'blur' : 'change' }]"
 					>
-						<template v-if="val.type !== ''">
-							<el-form-item
-								:label="$t(val.label)"
-								:prop="val.prop"
-								:rules="[{ required: val.required, message: `${val.label}不能为空`, trigger: val.type === 'input' ? 'blur' : 'change' }]"
-							>
-								<span style="width: 100%; font-weight: 700; color: #1890ff">
-									{{ state.form[val.prop] }}
-								</span>
-							</el-form-item>
-						</template>
-					</el-col>
-				</el-row>
-			</el-form>
-		</el-card>
-	</div>
+						<span style="width: 100%; font-weight: 700; color: #1890ff">
+							{{ state.form[val.prop] }}
+						</span>
+					</el-form-item>
+				</template>
+			</el-col>
+		</el-row>
+	</el-form>
+	<el-empty v-else description="数据出错" />
+	<!-- </el-card> -->
+	<!-- </div> -->
 </template>
 
 <script setup lang="ts" name="/partno/noSearch">
 import { useRoute, useRouter } from 'vue-router';
-import { defineAsyncComponent, reactive, ref, onMounted } from 'vue';
+import { defineAsyncComponent, reactive, ref, onMounted, watch } from 'vue';
 import { ElMessage } from 'element-plus';
 import { getMaterialApi } from '/@/api/link/noSearchLink';
 import { useI18n } from 'vue-i18n';
 
 const route = useRoute();
 const router = useRouter();
+// 定义父组件传过来的值
+const props = defineProps({
+	isDialog: {
+		type: Boolean,
+		default: () => false,
+	},
+	matNoRef: {
+		type: String,
+		default: () => '',
+	},
+});
 const state = reactive<LinkState>({
 	search: [
-		{ label: '料号：', prop: 'matNo', type: 'text' },
+		{ label: '料号：', prop: 'matNo', type: 'text', xs: 24, sm: 24, md: 24, lg: 24, xl: 24 },
 		{ label: '中文：', prop: 'nameCh', type: 'text' },
 		{ label: '英文：', prop: 'nameEn', type: 'text' },
 		{ label: '图纸编号：', prop: 'drawNo', type: 'text' },
@@ -55,7 +67,6 @@ const state = reactive<LinkState>({
 		{
 			label: '图纸文件：',
 			prop: 'drawPath',
-
 			type: 'text',
 			xs: 24,
 			sm: 24,
@@ -89,11 +100,21 @@ const state = reactive<LinkState>({
 	searchConfig: false,
 	form: {},
 });
+watch(
+	() => props.matNoRef,
+	() => {
+		getSelect();
+	}
+);
 // 下拉框数据
 const getSelect = async () => {
-	const res = await getMaterialApi(route.query.comkey);
-	console.log(route.query);
+	// link/noSearchLink?comkey=CSG2023243-DP-56321-003
+	let comkey = props.isDialog ? props.matNoRef : route.query.comkey;
+	const res = await getMaterialApi(comkey);
 	state.form = res.data;
+	if (!res.status) {
+		state.search = [];
+	}
 };
 // 页面加载时
 onMounted(() => {
